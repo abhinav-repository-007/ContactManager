ea6c7b7a1c23086db831db3038a33f42a9eac3f1
u23490 <66670675+u23490@users.noreply.github.com>
2021-04-09 10:07:44 +0530

    add chnange password module

--- src/main/java/com/smart/smartcontactmanager/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/com/smart/smartcontactmanager/controller/UserController.java
@@ -3,14 +3,14 @@ package com.smart.smartcontactmanager.com.smart.smartcontactmanager.controller;
 import com.smart.smartcontactmanager.com.smart.smartcontactmanager.dao.UserRepository;
 import com.smart.smartcontactmanager.com.smart.smartcontactmanager.entities.Contact;
 import com.smart.smartcontactmanager.com.smart.smartcontactmanager.entities.User;
+import com.smart.smartcontactmanager.com.smart.smartcontactmanager.helper.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @Controller
@@ -18,6 +18,8 @@ import java.security.Principal;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @ModelAttribute
     public void addCommanData(Model model,Principal principal){
@@ -56,9 +58,39 @@ public class UserController {
       contact.setUser(user);
        user.getContacts().add(contact);
        this.userRepository.save(user);
-
         System.out.println("DATA_ "+contact);
 
         return "add_contact_form";
     }
+
+    //open setting handler
+    @GetMapping("/settings")
+    public String openSetting(){
+
+        return "settings";
+    }
+
+    @PostMapping("/change-password")
+    public String changePassword(@RequestParam("oldPassword") String oldPassword,
+                                 @RequestParam("newPassword")String newPassword,
+                                 Principal principal,HttpSession session){
+        System.out.println("OLD password "+oldPassword);
+        System.out.println("new password "+newPassword);
+        String name =principal.getName();
+
+        User user=   this.userRepository.getUserByUserName(name);
+        System.out.println(user.getPassword());
+       if(this.passwordEncoder.matches(oldPassword,user.getPassword())){
+           user.setPassword(this.passwordEncoder.encode(newPassword));
+
+           this.userRepository.save(user);
+           session.setAttribute("message",new Message("Password succefully chnaged!!","alert-success"));
+       }else {
+           session.setAttribute("message",new Message("Please enter correct old password!","danger"));
+       }
+
+        return "redirect:/user/index";
+    }
+
+
 }
